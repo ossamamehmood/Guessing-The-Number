ad82cb152d475d4dd1a3f8472a94e235ba141e0d
Ossama Mehmood <40134503+ossamamehmood@users.noreply.github.com>
2020-10-26 23:53:51 +0500

    Add files via upload

---
+++ Guess the Number.py
@@ -0,0 +1,22 @@
+answer = 23
+
+attempts = 0
+
+entry = ""
+
+''' a loop that repeats while the users guess is not the same as the answer '''
+
+print("\n\t\t Guess the Number - Game")
+
+
+while answer != entry :
+
+    entry = int(input("\n\t   Enter a number between 1 and 100 : "))
+    
+    ''' Each time through the loop 1 is added to the number of attempts '''
+    
+    attempts = attempts + 1
+    
+''' After the loop it will say how many attempts it took '''
+
+print("\nWell done you correctly guessed the number it took you " + str(attempts) + " attempts")
